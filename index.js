072040f5f7ce3ccc62d08760ec4af6f871598414
steep-47 <2157528478@qq.com>
2026-09-23 02:00:30 +0800

    Fix day theme prompt manager contrast

--- index.js
+++ index.js
@@ -306,7 +306,7 @@ function init() {
             .filter(Boolean)
             .forEach((event) => ctx.eventSource.on(event, () => setTimeout(() => refreshMessages(), 0)));
     }
-    console.info('[UI-N] v0.3.2 loaded');
+    console.info('[UI-N] v0.3.3 loaded');
 }
 
 if (document.readyState === 'loading') {
