cd1974ed2dd3fe2202b37781dd90cd78fa7fb78e
steep-47 <2157528478@qq.com>
2026-09-23 01:32:52 +0800

    Improve swipe controls visibility

--- index.js
+++ index.js
@@ -296,7 +296,7 @@ function init() {
             .filter(Boolean)
             .forEach((event) => ctx.eventSource.on(event, () => setTimeout(() => refreshMessages(), 0)));
     }
-    console.info('[UI-N] v0.1.3 loaded');
+    console.info('[UI-N] v0.1.4 loaded');
 }
 
 if (document.readyState === 'loading') {
